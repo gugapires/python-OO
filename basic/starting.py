6820ea570bbe093fff548d46ffea57c0584146b3
yranda <gustavomiranda215@gmail.com>
2018-08-25 20:41:54 -0300

    um dos motivos para se usar classes e instancias

--- basic/starting.py
+++ basic/starting.py
@@ -1,3 +1,26 @@
+# Aqui vamos falar um pouco de classes e instancias
+
+class empregado:
+    pass
 
 
-print("hello")
+emp_1= empregado()
+emp_2= empregado()
+
+print(emp_1)
+print(emp_2)
+
+
+emp_1.first = "bunda"
+emp_1.last = "mole"
+emp_1.email = "bunda.mole@gmail.com"
+emp_1.pay = 50
+
+
+emp_2.first = "jon"
+emp_2.last = "nit"
+emp_2.email = "jon.nit@gmail.com"
+emp_2.pay = 70
+
+print(emp_1.email)
+print(emp_2.email)
