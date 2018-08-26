1fc47819ef0ef81f0ab780bad5eba5aef00e892e
yranda <gustavomiranda215@gmail.com>
2018-08-25 21:00:24 -0300

    alterando nome dos arquivos

---
+++ basic/vantagem_classe.py
@@ -0,0 +1,8 @@
+# Vantagens de usar classes e instancias.
+# Convencinou-se chamar de self, porém pode chama-la do que quiser.
+
+class empregado:
+    pass
+
+    # pode ser comparada a um construtor
+    def __init__(self):
