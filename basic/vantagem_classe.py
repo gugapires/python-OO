d6c16da78fe7cfd324528a45973697ef2e76544f
yranda <gustavomiranda215@gmail.com>
2018-08-25 21:26:20 -0300

    Rever o final da aula 1 de python orientado a objeto em "https://www.youtube.com/watch?v=ZDa-Z5JzLYM&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc"

--- basic/vantagem_classe.py
+++ basic/vantagem_classe.py
@@ -4,5 +4,26 @@
 class empregado:
     pass
 
-    # pode ser comparada a um construtor
-    def __init__(self):
+    # É um método de inicialização, pode encara-la como um construtor
+    # depois do self, podemos especificar os demais argumentos que queremos
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+
+    def fullname(self):
+        return '{} {}'.format(emp_1.first, emp_1.last)
+
+
+
+# Passamos os paramentos para as duas instancias a baixo
+emp_1= empregado('bunda','mole', 50)
+emp_2= empregado('jon','nit', 70)
+
+emp_1.fullname()
+#
+empregado.fullname(emp_1)
+# quando passamos a classe, ela não sabe o que exatamente ira rodar
+# logo, para rodar um metodo temos que passar uma instancia
